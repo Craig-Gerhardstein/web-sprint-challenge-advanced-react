fc1792b548fe6c86068c4c3f30049c498fa33565
Craig Gerhardstein <77256669+Craig-Gerhardstein@users.noreply.github.com>
2021-03-12 04:53:29 -0500

    mvp

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,71 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+  //Arrange
+  const checkoutFormHeader = screen.getByText(/Checkout Form/i);
+  const firstName = screen.getByText(/First Name/i);
+  const lastName = screen.getByText(/Last Name/i);
+  const address = screen.getByText(/Address/i);
+  const city = screen.getByText(/City/i);
+  const state = screen.getByText(/State/i);
+  const zip = screen.getByText(/Zip/i);
+  const button = screen.getByRole("button", { name: /checkout/i });
 
-test("form shows success message on submit with form details", () => {});
+  //Act
+  expect(checkoutFormHeader).toBeInTheDocument();
+  expect(firstName).toBeInTheDocument();
+  expect(lastName).toBeInTheDocument();
+  expect(address).toBeInTheDocument();
+  expect(city).toBeInTheDocument();
+  expect(state).toBeInTheDocument();
+  expect(zip).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
+
+  //Assert
+  expect(checkoutFormHeader).toBeVisible();
+  expect(firstName).toBeVisible();
+  expect(lastName).toBeVisible();
+  expect(address).toBeVisible();
+  expect(city).toBeVisible();
+  expect(state).toBeVisible();
+  expect(zip).toBeVisible();
+  expect(button).toBeVisible();
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+  
+    //Arrange
+    const firstName = screen.getByLabelText(/First Name/i);
+    const lastName = screen.getByLabelText(/Last Name/i);
+    const address = screen.getByLabelText(/Address/i);
+    const city = screen.getByLabelText(/City/i);
+    const state = screen.getByLabelText(/State/i);
+    const zip = screen.getByLabelText(/Zip/i);
+    const button = screen.getByRole("button", { name: /checkout/i });
+  
+    //Act
+    userEvent.type(firstName, "Craig");
+    userEvent.type(lastName, "Gerhardstein");
+    userEvent.type(address, "12480 Roadway");
+    userEvent.type(city, "ComeCity");
+    userEvent.type(state, "OH");
+    userEvent.type(zip, "45233");
+    userEvent.click(button);
+  
+    //Assert
+    const orderProcessedMessage = screen.getByText(/green friends/i);
+    const customerDetails = screen.getByText(/Craig/i);
+  
+    expect(orderProcessedMessage).toBeTruthy();
+    expect(customerDetails).toBeTruthy();
+  
+    expect(orderProcessedMessage).toBeVisible();
+    expect(customerDetails).toBeVisible();
+  });
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,10 +3,26 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  state = {
+    plants: [],
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    console.log("Log From componentDidMount");
+    axios
+      .get(`http://localhost:3333/plants`)
+      .then((res) => {
+        console.log("Initial Data Value From Axios Request", res.data);
+        
+        this.setState((prevState) => ({ ...prevState, plants: res.data }));
+      })
+      .catch((error) => {
+        console.log("API CAlL ERROR FROM COMPONENT DIO MOUNT", error);
+      });
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
